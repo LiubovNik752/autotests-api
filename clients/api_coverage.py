31c36176e5f5632636f1f9b4f72fb1d604ed4afd
Lyubov Baturina <liubovnik757@gmail.com>
2026-01-18 19:34:27 +0300

    Практикуемся в интеграции покрытия в API тесты

---
+++ clients/api_coverage.py
@@ -0,0 +1,7 @@
+import httpx
+from swagger_coverage_tool import SwaggerCoverageTracker
+
+# Инициализируем трекер для сервиса с ключом "api-service"
+tracker = SwaggerCoverageTracker(service="api-course")
+
+
